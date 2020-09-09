a877b68e995a0671f198aa07809325fe50476f73
Anes-Marzooki <marzoukianas@gmail.com>
2020-09-09 16:28:14 +0100

    create a new instance

--- src/model/Book.js
+++ src/model/Book.js
@@ -1,6 +1,14 @@
 // book constructor
-Book(slots) => {
+function Book(slots) {
   this.title = slots.title;
   this.year = slots.year;
   this.isbn = slots.isbn;
+}
+
+Book.instances = {};
+// creates a new instance and adds it to Book.instances coll
+Book.add = (slots) {
+  let book = new Book(slots);
+  Book.instances[slots.isbn] = book;
+  console.log("Book" + " " + slots.isbn + " created!");
 }
